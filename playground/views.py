89f19bf174e8979ddefc5b697c2ff5b792ee7c87
William Jiongwei Zhu <jiongwei_zhu@techpass.gov.sg>
2023-07-12 18:27:03 +0800

    add sleep in the response to simulate the real testing case

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,10 @@
+import time
+
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def say_hello(request):
+    time.sleep(2)
     return render(request, 'hello.html')
 
 
